83705dd7bcc7fc279dc883722471bc9736ceafbd
Benjamin Summers <benjamin@tlon.io>
2019-01-14 14:42:30 -0800

    Build `libsni` without Meson.

--- nix/sniproxy.nix
+++ nix/sniproxy.nix
@@ -2,10 +2,5 @@
 
 pkgs.stdenv.mkDerivation rec {
   name              = "sniproxy";
-  nativeBuildInputs = with pkgs; [ meson ninja pkgconfig ];
   src               = ../vendor/sniproxy;
-  preInstall = ''
-    mkdir -p $out/include
-    cp $src/src/tls.h $out/include
-  '';
 }
