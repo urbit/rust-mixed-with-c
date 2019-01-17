fd770a0df9cb2fd9527a4ccb47977b28f30719a4
Benjamin Summers <benjamin@tlon.io>
2019-01-16 18:13:59 -0800

    Faster nixpkgs fetch?

--- default.nix
+++ default.nix
@@ -1,10 +1,10 @@
 let
 
   nixpkgs =
-    builtins.fetchGit {
+    builtins.fetchTarball {
       name = "nixpkgs-2019-01-15";
-      url = https://github.com/nixos/nixpkgs/;
-      rev = "368b72a71dee920c79d98b27b524d62550a347ec";
+      url = https://github.com/nixos/nixpkgs/archive/368b72a71dee920c79d98b27b524d62550a347ec.tar.gz;
+      sha256 = "0m970cy8366sdiw90345f1fa2zyicd5vinmjljfxg0nx9p934rcp";
     };
 
 in
