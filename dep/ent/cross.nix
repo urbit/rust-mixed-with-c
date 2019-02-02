2c2fd8eecc982745b01c70f4d19e02115bc983a6
Benjamin Summers <benjamin@tlon.io>
2019-02-01 16:42:25 -0800

    Got cross compilation working for libent.

--- dep/ent/cross.nix
+++ dep/ent/cross.nix
@@ -1,18 +1,10 @@
 { crossenv }:
 
 crossenv.make_derivation rec {
-  name          = "ent-7506f";
-  native_inputs = with crossenv.nixpkgs; [ meson ninja pkgconfig ];
-  builder       = ./builder.sh;
+  name    = "ent-7506f";
+  builder = ./builder.sh;
+  src     = ./src;
 
-  mesonBuildType = "release";
-
-  mesonFlags = "-Dent_install=true";
-
-  src = crossenv.nixpkgs.fetchFromGitHub {
-    owner = "urbit";
-    repo = "libent";
-    rev = "fe9f8a8aad6c4a6915ff8844850e64be4b4831a2";
-    sha256 = "1pffs9mr8p0flk11qypnqfq54ijgci60iw5d7qmrckf0cjb00nn0";
-  };
+  CC = "${crossenv.host}-gcc";
+  AR = "${crossenv.host}-ar";
 }
--- dep/ent/default.nix
+++ dep/ent/default.nix
@@ -1,17 +1,7 @@
 { pkgs }:
 
 pkgs.stdenv.mkDerivation rec {
-  name        = "ent-7506f";
-  buildInputs = [ pkgs.meson pkgs.ninja pkgs.pkgconfig ];
-  builder     = ./builder.sh;
-
-  mesonBuildType = "release";
-  mesonFlags     = "-Dent_install=true";
-
-  src = pkgs.fetchFromGitHub {
-    owner = "urbit";
-    repo = "libent";
-    rev = "fe9f8a8aad6c4a6915ff8844850e64be4b4831a2";
-    sha256 = "1pffs9mr8p0flk11qypnqfq54ijgci60iw5d7qmrckf0cjb00nn0";
-  };
+  name    = "ent-7506f";
+  builder = ./builder.sh;
+  src     = ./src;
 }
