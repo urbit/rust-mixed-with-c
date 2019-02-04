6d59ed64bab6de6914ae5c48d4842629b1373474
Benjamin Summers <benjamin@tlon.io>
2019-02-04 11:06:42 -0800

    urbit: Simpler build system.

--- pkg/urbit/release.sh
+++ pkg/urbit/release.sh
@@ -9,36 +9,17 @@ cp -r $src ./src
 chmod -R u+w ./src
 cd src
 
-export PKG_CONFIG_LIBDIR=$PKG_CONFIG_CROSS_PATH
-export PKG_CONFIG_PATH=$PKG_CONFIG_CROSS_PATH
-
-export CFLAGS
-export LDFLAGS
-
-for dep in $argon2 \
-           $curl \
-           $ed25519 \
-           $ent \
-           $h2o \
-           $libgmp \
-           $libsigsegv \
-           $murmur3 \
-           $ncurses \
-           $openssl \
-           $scrypt \
-           $secp256k1 \
-           $sni \
-           $softfloat3 \
-           $uv \
-           $zlib
-do find $dep/{lib,include} -type f
-   CFLAGS="$CFLAGS -I$dep/include"
-   LDFLAGS="$LDFLAGS -L$dep/lib"
+for dep in $cross_inputs; do
+   export CFLAGS="$CFLAGS -I$dep/include"
+   export LDFLAGS="$LDFLAGS -L$dep/lib"
 done
 
-CFLAGS="$CFLAGS -I$ncurses/include/ncurses"
+export PKG_CONFIG=pkg-config-cross
+export HOST=$host
 
-./build.sh
+./configure
+
+make urbit -j8
 
 mkdir -p $out/bin
 cp urbit $out/bin/$exename
